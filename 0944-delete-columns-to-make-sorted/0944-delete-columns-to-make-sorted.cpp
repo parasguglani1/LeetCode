8b9fcd1bb0f94b2b8dfef7013a272410ba7347bd
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-01-03 14:37:47 +0530

    Time: 59 ms (62.73%), Space: 12.2 MB (30.23%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -4,29 +4,19 @@ public:
         int n=strs.size();
         int m=strs[0].size();
         int count=0;
-        int i=0,j=0;
-        
-        
-            while(j<m)
+        for(int j=0;j<m;j++)
+        {
+            for(int i=0;i<n-1;i++)
             {
                 if(strs[i][j]>strs[i+1][j])
                 {
                     count++;
-                    j++;
-                    i=0;
+                    break;
                 }
-            else
-            {
-                i++;
-                if(i==n-1)
-                {
-                    j++;
-                    i=0;
-                }
-            }
+            
                
             }
-        
+        }
         return count;
     }
 };
