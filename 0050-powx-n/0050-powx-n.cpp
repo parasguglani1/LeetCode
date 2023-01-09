3c253976a258f3bd10eea6443a76c2f729494f60
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-01-09 15:42:00 +0530

    Time: 4 ms (35.60%), Space: 5.9 MB (89.11%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,9 +1,35 @@
 class Solution {
 public:
     double myPow(double x, int n) {
- return pow(x,n);
-            
+        double ans=1.0;
+        bool neg=false;
+        if (n<0)
+        {
+            neg=true;
+        }
+        n=abs(n);
+        long long exp=n;
+        
+        while(exp>0)
+        {
+            if(exp&1)
+            {
+                ans=ans*x;
+                exp-=1;
+                
+            }
+            else
+            {
+                x*=x;
+                exp/=2;
+                
+            }
+        }
+        if(neg)
+        {
+            ans= 1.0/ans;
         }
         
-    
+        return ans;
+    }
 };
