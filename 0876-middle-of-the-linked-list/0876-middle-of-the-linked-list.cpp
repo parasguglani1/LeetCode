1c4e13a1e85b382640f312eb2f9128ef611185d9
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-01-18 18:47:28 +0530

    Time: 5 ms (28.36%), Space: 7.2 MB (27.60%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -1,31 +1,34 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *    int val;
+ *    ListNode * next;
+ *    ListNode() : val(0), next(nullptr) {}
+ *    ListNode(int x) : val(x), next(nullptr) {}
+ *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *};
  */
-class Solution {
-public:
-    ListNode* middleNode(ListNode* head) {
-        ListNode* temp1=head;
-        ListNode* temp2=head;
-        if(head->next==nullptr)
-        {return head;}
-        do
-        { temp1=temp1->next;
-          
-            temp2=temp2->next->next;
-            if(!temp2)
+class Solution
+{
+    public:
+        ListNode* middleNode(ListNode *head)
+        {
+            ListNode *temp1 = head;
+            ListNode *temp2 = head;
+            if (head->next == nullptr)
             {
-                return temp1;
+                return head;
             }
+            do {
+                temp1 = temp1->next;
+
+                temp2 = temp2->next->next;
+                if (!temp2)
+                {
+                    return temp1;
+                }
+            }
+            while (temp1 && temp2 && temp2->next);
+            return temp1;
         }
-        while(temp1&& temp2&&temp2->next);
-    return temp1;
-        
-    }
 };
