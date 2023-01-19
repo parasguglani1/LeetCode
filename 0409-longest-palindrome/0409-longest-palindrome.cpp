18cb6dfcb49138b7941f31953ee9a4a568fb7743
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-01-20 01:23:15 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (52.82%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        unordered_map<char,int> map;
-        for(int i=0;i<s.length();i++){
-            map[s[i]]++;
-        }
-        int cnt=0;
-        for(auto i:map){
-            if(i.second%2!=0){
-                cnt++;
-            }
-        }
-        if(cnt>1){
-            return s.length()-cnt+1;
-        }
-        return s.length();
+     bitset<64> bits(0);
+    for (char c : s) bits.flip(c - 'A');
+    return s.size() - bits.count() + bits.any();
+
         
     }
 };
