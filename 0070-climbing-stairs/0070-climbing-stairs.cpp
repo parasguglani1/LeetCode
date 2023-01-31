68802dd8bed0c3f9b9b0589acb82c6f1bccf4167
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-01-31 21:54:39 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (72.51%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+      if(n<=2)
+      {
+          return n;
+      }
+        int first=1,second=2;
+        int ans=2;
+        for(int i=3;i<=n;i++)
+        {
+            ans=first+second;
+            first=second;
+            second=ans;
+        }
+        return ans;
+        
+    }
+};
