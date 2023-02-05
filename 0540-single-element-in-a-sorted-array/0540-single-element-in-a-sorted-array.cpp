7292894fe586ae55389f3a5178690dfed0724d32
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-02-05 23:11:56 +0530

    Time: 27 ms (77.70%), Space: 22.5 MB (15.64%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+   int singleNonDuplicate(vector<int> &nums)
+{
+    int n = nums.size();
+    int start = 0;
+    int end = n - 1;
+    int mid;
+    // first element would always be on even index on left side and odd on right side
+    while (start <= end)
+    {
+        mid = start + (end - start) / 2;
+        int firstindex = mid, lastindex = mid;
+        if (firstindex > 0 && nums[firstindex] == nums[firstindex - 1])
+        {
+            firstindex--;
+        }
+        if (lastindex < n - 1 && nums[lastindex] == nums[lastindex + 1])
+        {
+            lastindex++;
+        }
+        if ((lastindex - firstindex) % 2 == 0)
+        {
+            return nums[firstindex];
+        }
+        if (firstindex % 2 == 0)
+        {
+            start = lastindex + 1;
+        }
+        else
+        {
+            end = firstindex - 1;
+        }
+    }
+    return nums[start];
+}
+};
