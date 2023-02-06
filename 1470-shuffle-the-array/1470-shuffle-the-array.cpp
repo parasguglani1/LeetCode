4b63e4b88eeea8884deaf2b9c3a10c668e60c396
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-02-06 11:56:26 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (31.98%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -3,20 +3,12 @@ class Solution
     public:
         vector<int> shuffle(vector<int> &nums, int n)
         {
-            int first = 0, second = n, max = 1001;
-            for (int i = 0; i < 2 * n; i++)
+            vector<int> v;
+            for (int i = 0; i < n; i++)
             {
-                if (i % 2 == 0)
-                {
-                    nums[i] = (nums[first++] % max) *max + nums[i];
-                }
-                else
-                {
-                    nums[i] = (nums[second++] % max) *max + nums[i];
-                }
+                v.push_back(nums[i]);
+                v.push_back(nums[n + i]);
             }
-            for (int i = 0; i < 2 * n; i++)
-                nums[i] /= max;
-            return nums;
+            return v;
         }
 };
