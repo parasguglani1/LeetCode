74c1cb0e75db2fd2979f28dd9d66d25ea550a274
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-02-14 01:01:46 +0530

    Time: 3 ms (58.22%), Space: 5.9 MB (64.52%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,35 +1,35 @@
-class Solution {
-public:
-    int reverse(int x) {
-        bool neg=false;
-        if(x<0)
+class Solution
+{
+    public:
+        int reverse(int x)
         {
-             neg=true;
-        }
-        int ans=0;
-        x=abs(x);
-        while(x>0)
-        {
-            if(ans>INT_MAX/10)
+            bool neg = false;
+            if (x < 0)
             {
-                return 0;
+                neg = true;
             }
-            ans*=10;   
-            ans+=x%10;
-         
-            x/=10;
-            if(ans<0)
+            int ans = 0;
+            x = abs(x);
+            while (x > 0)
             {
-                return 0;
+                if (ans > INT_MAX / 10)
+                {
+                    return 0;
+                }
+                ans *= 10;
+                ans += x % 10;
+
+                x /= 10;
+                // if (ans < 0)
+                // {
+                //     return 0;
+                // }
             }
+
+            if (neg)
+            {
+                ans *= -1;
+            }
+            return ans;
         }
-        
-        if(neg)
-        {
-            ans*=-1;
-        }
-        return ans;
-        
-        
-    }
 };
