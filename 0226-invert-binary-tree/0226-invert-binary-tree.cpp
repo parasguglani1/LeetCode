db355ac080e1d0d331283e112e9a476acab21ce0
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-02-18 14:29:04 +0530

    Time: 0 ms (100.00%), Space: 9.8 MB (46.85%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -1,33 +1,31 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *int val;
+ *TreeNode * left;
+ *TreeNode * right;
+ *TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    void invert(TreeNode* root)
-    {
-        if(!root)
+class Solution
+{
+    public:
+        TreeNode* invertTree(TreeNode *root)
         {
-            return;
+            if (!root)
+            {
+                return root;
+            }
+
+            invertTree(root->left);
+            invertTree(root->right);
+
+            TreeNode *temp = root->left;
+            root->left = root->right;
+            root->right = temp;
+
+            return root;
         }
-        invert(root->left);
-        invert(root->right);
-        
-        TreeNode* temp=root->left;
-        root->left=root->right;
-        root->right=temp;
- 
-        
-    }
-    TreeNode* invertTree(TreeNode* root) {
-        invert(root);
-        return root;
-    }
 };
