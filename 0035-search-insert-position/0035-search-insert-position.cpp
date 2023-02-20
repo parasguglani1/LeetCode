66876783506ddb9837ddd6606a417ac105743fc1
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-02-20 15:09:04 +0530

    Time: 3 ms (90.12%), Space: 9.7 MB (31.02%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -3,34 +3,34 @@ class Solution
     public:
         int searchInsert(vector<int> &nums, int target)
         {
-            // int l = 0;
-            // int n = nums.size();
-            // if (nums[n - 1] < target)
-            // {
-            //     return n;
-            // }
-            // if (nums[0] > target)
-            // {
-            //     return 0;
-            // }
-            // int r = n - 1;
-            // while (l <= r)
-            // {
-            //     int mid = l + (r - l) / 2;
-            //     if (nums[mid] == target)
-            //     {
-            //         return mid;
-            //     }
-            //     else if (nums[mid] < target)
-            //     {
-            //         r = mid-1 ;
-            //     }
-            //     else
-            //     {
-            //         l = mid + 1;
-            //     }
-            // }
-            // return l;
-            return (lower_bound(nums.begin(),nums.end(),target)-nums.begin());
+            int l = 0;
+            int n = nums.size();
+            if (nums[n - 1] < target)
+            {
+                return n;
+            }
+            if (nums[0] > target)
+            {
+                return 0;
+            }
+            int r = n - 1;
+            while (l <= r)
+            {
+                int mid = l + (r - l) / 2;
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+                else if (nums[mid] < target)
+                {
+                    l = mid+1 ;
+                }
+                else
+                {
+                    r = mid - 1;
+                }
+            }
+            return l;
+            // return (lower_bound(nums.begin(),nums.end(),target)-nums.begin());
         }
 };
