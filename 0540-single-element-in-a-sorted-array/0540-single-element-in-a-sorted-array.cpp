3bf0f7258a13cdd647451d5788816a932f48016d
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-02-21 13:55:49 +0530

    Time: 30 ms (55.07%), Space: 22.3 MB (93.44%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,40 +1,32 @@
-class Solution {
-public:
-
-int singleNonDuplicate(vector<int> &nums)
+class Solution
 {
-    int n = nums.size();
-    int start = 0;
-    int end = n - 1;
-    int mid;
-    // first element would always be on even index on left side and odd on right side
-    while (start <= end)
-    {
-        mid = start + (end - start) / 2;
-        // int firstindex = mid, lastindex = mid;
-        int firstindex = lower_bound(nums.begin(), nums.end(), nums[mid]) - nums.begin();
-        int lastindex = upper_bound(nums.begin(), nums.end(), nums[mid]) - nums.begin() - 1;
-        // if (firstindex > 0 && nums[firstindex] == nums[firstindex - 1])
-        // {
-        //     firstindex--;
-        // }
-        // if (lastindex < n - 1 && nums[lastindex] == nums[lastindex + 1])
-        // {
-        //     lastindex++;
-        // }
-        if ((lastindex - firstindex) % 2 == 0)
+    public:
+
+        int singleNonDuplicate(vector<int> &nums)
         {
-            return nums[firstindex];
+            int n = nums.size();
+            int start = 0;
+            int end = n - 1;
+            int mid;
+            while (start <= end)
+            {
+                mid = start + (end - start) / 2;
+                int firstindex = lower_bound(nums.begin(), nums.end(), nums[mid]) - nums.begin();
+                int lastindex = upper_bound(nums.begin(), nums.end(), nums[mid]) - nums.begin() - 1;
+
+                if ((lastindex - firstindex) % 2 == 0)
+                {
+                    return nums[firstindex];
+                }
+                if (firstindex % 2 == 0)
+                {
+                    start = lastindex + 1;
+                }
+                else
+                {
+                    end = firstindex - 1;
+                }
+            }
+            return nums[start];
         }
-        if (firstindex % 2 == 0)
-        {
-            start = lastindex + 1;
-        }
-        else
-        {
-            end = firstindex - 1;
-        }
-    }
-    return nums[start];
-}
 };
