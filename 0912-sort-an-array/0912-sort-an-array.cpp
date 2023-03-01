7023285c5e7c13f804aceb52e95fdc0a17b992b4
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-03-01 10:10:49 +0530

    Time: 195 ms (64.36%), Space: 65.9 MB (38.21%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+    public:
+        vector<int> sortArray(vector<int> &nums)
+        {
+            priority_queue<int, vector < int>, greater < int>> pq;
+            for (int i: nums)
+                pq.push(i);
+            for (int i = 0; i < nums.size(); i++)
+            {
+                nums[i] = pq.top();
+                pq.pop();
+            }
+            return nums;
+        }
+};
