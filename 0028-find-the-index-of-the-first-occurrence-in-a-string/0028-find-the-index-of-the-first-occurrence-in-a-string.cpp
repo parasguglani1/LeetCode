dab13d254e79953608346279d36203b8fa9c4d34
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-03-03 22:43:20 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (69.99%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        auto it=haystack.find(needle);
-        if(it==string::npos)
-        {
-            return -1;
-        }
-        return it;
+        
+        
+       
+        return haystack.find(needle)==string::npos?-1:haystack.find(needle);
     }
 };
