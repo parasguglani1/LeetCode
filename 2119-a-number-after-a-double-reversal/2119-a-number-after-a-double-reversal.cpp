e13c59b1504a4f99b4f557c0152482efd4c4127a
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-03-06 00:48:38 +0530

    Time: 0 ms (100.00%), Space: 6 MB (5.37%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     bool isSameAfterReversals(int num) {
-        string s1=to_string(num);
-        reverse(s1.begin(),s1.end());
-        int num2=stoi(s1);
-        string s2=to_string(num2);
-        reverse(s2.begin(),s2.begin());
-        return s1==s2;
+     
+        return num==0|| !(num%10==0);
     }
 };
