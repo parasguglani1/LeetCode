82082c10e19bfdb132bcac2af34ccac08eed8cb0
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-03-09 19:26:18 +0530

    Time: 3 ms (67.10%), Space: 6 MB (63.25%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,21 @@
-class Solution {
-public:
-    int mySqrt(int x) {
-     return pow(x,0.5);
-    }
+class Solution
+{
+    public:
+        int mySqrt(int x)
+        {
+            long long l = 1, r = (long)x +1;
+            while (l < r)
+            {
+                long long mid = l + (r - l) / 2;
+                if (mid  <= x/mid)
+                {
+                    l = mid + 1;
+                }
+                else
+                {
+                    r = mid;
+                }
+            }
+            return l - 1;
+        }
 };
