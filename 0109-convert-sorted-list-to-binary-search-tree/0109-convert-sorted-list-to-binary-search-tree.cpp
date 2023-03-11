3ef80e97c7e240a078d30fa8611214a6e449279a
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-03-11 17:48:06 +0530

    Time: 24 ms (84.58%), Space: 28.3 MB (92.35%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
@@ -1,50 +1,49 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *    int val;
+ *    ListNode * next;
+ *    ListNode() : val(0), next(nullptr) {}
+ *    ListNode(int x) : val(x), next(nullptr) {}
+ *    ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    
-    TreeNode* buildTree(int st,int end,vector<int> &v)
+class Solution
+{
+    public:
+
+        TreeNode* buildTree(ListNode *head, ListNode *tail)
+        {
+            if (head == tail)
+            {
+                return NULL;
+            }
+            ListNode *mid = head, *temp = head;
+            while (temp != tail && temp->next != tail)
+            {
+                mid = mid->next;
+                temp = temp->next->next;
+            }
+
+            TreeNode *root = new TreeNode(mid->val);
+            root->left = buildTree(head, mid );
+            root->right = buildTree(mid ->next, tail);
+            return root;
+        }
+    TreeNode* sortedListToBST(ListNode *head)
     {
-        if(st>end)
-        {
-            return nullptr;
-        }
-        int mid=(st+end)/2;
-        TreeNode* root=new TreeNode(v[mid]);
-        root->left=buildTree(st,mid-1,v);
-        root->right=buildTree(mid+1,end,v);
-        return root;
-        
-    }
-    TreeNode* sortedListToBST(ListNode* head) {
-        vector<int> v;
-        auto temp=head;
-        while(temp)
-        {
-            v.push_back(temp->val);
-            temp=temp->next;
-        }
-        int n=v.size();
-        return buildTree(0,n-1,v);
-        
+
+        return buildTree(head, NULL);
     }
 };
