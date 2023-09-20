b8eb3fb814d0d8400ceda32a89a7c7a23be635a9
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-09-20 22:28:48 +0530

    Time: 1015 ms (5.05%), Space: 294 MB (5.04%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1658-minimum-operations-to-reduce-x-to-zero/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1658-minimum-operations-to-reduce-x-to-zero/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,59 +1,60 @@
-class Solution {
-public:
-    int minOperations(vector<int>& nums, int x) {
-        int n=nums.size();
-        vector<int> pref(n+1),suff(n+1);
-        pref[0]=nums[0];
-        map<int,int> pmp,smp;
-        for(int i=1;i<n;i++)
+class Solution
+{
+    public:
+        int minOperations(vector<int> &nums, int x)
         {
-            pref[i]=pref[i-1]+nums[i];
-            pmp.insert({pref[i],i+1});
-        }
-        pmp.insert({pref[0],1});
-        suff[n-1]=nums[n-1];
-        smp.insert({suff[n-1],1});
-        
-        for(int i=n-2;i>=0;i--)
-        {
-            suff[i]=suff[i+1]+nums[i];
-            smp.insert({suff[i],n-i});
-        }
-        int ans=INT_MAX;
-        for(auto c:pmp)
-        {
-            if(c.first==x)
+            int n = nums.size();
+            // vector<int> pref(n + 1), suff(n + 1);
+            int sum = nums[0];
+            map<int, int> pmp, smp;
+            pmp.insert({ nums[0],1 });
+            
+
+            for (int i = 1; i < n; i++)
             {
-                ans=min(ans,c.second);
+                sum+= nums[i];
+                pmp.insert({ sum,
+                    i + 1 });
             }
-        }
-        cout<<ans<<endl;
-        for(auto c:smp)
-        {
-            if(c.first==x)
+            smp.insert({ nums[n-1],1 });
+            int sum2=nums[n-1];
+
+            for (int i = n - 2; i >= 0; i--)
             {
-                ans=min(ans,c.second);
+                sum2+=nums[i];
+                smp.insert({ sum2,
+                    n - i });
             }
-        }
-        // cout<<ans<<endl;
-        
-        for(auto c:pmp)
-        {
-            int fnd=x-c.first;
-//             modify this line
-            if(smp[fnd] &&c.second+smp[fnd]<n)
+            int ans = INT_MAX;
+            for (auto c: pmp)
             {
-                ans=min(ans,smp[fnd]+c.second);
+                if (c.first == x)
+                {
+                    ans = min(ans, c.second);
+                }
             }
+            for (auto c: smp)
+            {
+                if (c.first == x)
+                {
+                    ans = min(ans, c.second);
+                }
+            }
+
+            for (auto c: pmp)
+            {
+                int fnd = x - c.first;
+               	//             modify this line
+                if (smp[fnd] && c.second + smp[fnd] < n)
+                {
+                    ans = min(ans, smp[fnd] + c.second);
+                }
+            }
+           	// cout<<ans<<endl;
+            if (ans == INT_MAX)
+            {
+                return -1;
+            }
+            return ans;
         }
-        // cout<<ans<<endl;
-        if(ans==INT_MAX)
-        {
-            return -1;
-        }
-        return ans;
-        
-        
-        
-    }
 };
