a54a98a6fee52de671e17152157f8d3d7404c0c1
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-10-07 15:16:25 +0530

    Time: 129 ms (79.31%), Space: 109.8 MB (81.61%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2499-minimum-total-cost-to-make-arrays-unequal/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2499-minimum-total-cost-to-make-arrays-unequal.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2499-minimum-total-cost-to-make-arrays-unequal/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2499-minimum-total-cost-to-make-arrays-unequal.cpp
@@ -1,22 +1,44 @@
 class Solution {
 public:
     long long minimumTotalCost(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size(), total = 0;
-        long long ans = 0;
-        unordered_map<int, int> freq;
-        for (int i = 0; i < n; ++i)
-            if(nums1[i] == nums2[i]) {
-                ++freq[nums1[i]];
-                ++total; 
-                ans += i;
+        int n = nums1.size();
+        vector<bool>arr(n);//record pairs to change
+        vector<int>pairs(n+1);//record frequency of pairs of number
+        
+        int cnt = 0;//number of pairs to change
+        //build cnt and pairs
+        for(int i = 0; i < n; i++){
+            if(nums1[i] == nums2[i]){
+                arr[i] = true;
+                ++cnt;
+                ++pairs[nums1[i]];
             }
-        if (!total) return 0; 
-        auto [key, most] = *max_element(freq.begin(), freq.end(), [&](auto& lhs, auto& rhs) {return lhs.second < rhs.second;}); 
-        for (int i = 0; i < n && 2*most > total; ++i) 
-            if (nums1[i] != nums2[i] && key != nums1[i] && key != nums2[i]) {
-                ++total;
-                ans += i;
+        }
+
+        //find the most frequent paired number x and its frequency y
+        int x = -1, y = 0;
+        for(int i = 1; i <= n; i++){
+            if(pairs[i] > y){
+                y = pairs[i];
+                x = i;
             }
-        return 2*most > total ? -1 : ans; 
+        }
+        if(x == -1)return 0;
+
+        //for index i, add it into pairs to change if it doesn't have any x
+        int id = -1;
+        while(2*y > cnt){
+            ++id;
+            if(id == n)return -1;//x is so much that we can't fill it in the whole array
+            if(arr[id])continue;
+            if(nums1[id] == x || nums2[id] == x)continue;
+            ++cnt;
+            arr[id] = true;
+        }
+
+        //since we have found all pairs to change, we could simply add up all the indices
+        long long ret = 0;
+        for(int i = 0; i < n; i++)if(arr[i])ret += i;
+        return ret;
     }
 };
