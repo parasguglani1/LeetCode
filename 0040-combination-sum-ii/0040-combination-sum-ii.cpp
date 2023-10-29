3d952590cb126b4b959c0c63a9ab95d741407bf9
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-10-29 23:18:26 +0530

    Time: 10 ms (20.81%), Space: 11.3 MB (31.13%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -2,30 +2,57 @@ class Solution
 {
     public:
 
-       void findCombination(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds)
-{
-    if (target == 0)
+        void helper(int ind, int curr, int target, set<vector < int>> &ans, vector< int > &temp, vector< int > &candidates)
+        {
+            if (curr == target)
+            {
+               	// sort(temp.begin(),temp.end());
+                ans.insert(temp);
+                return;
+            }
+            if (ind == candidates.size())
+            {
+                return;
+            }
+           	//base
+            if (curr > target)
+            {
+                return;
+            }
+
+           	//take
+            for (int i = ind; i < candidates.size(); i++)
+            {
+                if (i == ind || candidates[i] != candidates[i - 1])
+                {
+                    curr += candidates[i];
+                    temp.push_back(candidates[i]);
+                    helper(i + 1, curr, target, ans, temp, candidates);
+
+                    curr -= candidates[i];
+
+                    temp.pop_back();
+                }
+            }
+        }
+
+    vector<vector < int>> combinationSum2(vector<int> &candidates, int target)
     {
-        ans.push_back(ds);
-        return;
+        set<vector < int>> ans;
+
+       	//take
+        int curr = 0;
+        sort(candidates.begin(), candidates.end());
+        int n = candidates.size();
+        for (auto x: candidates)
+        {
+           	// cout << x << endl;
+        }
+        vector<int> temp;
+        helper(0, curr, target, ans, temp, candidates);
+
+        vector<vector < int>> res;
+        res.assign(ans.begin(), ans.end());
+        return res;
     }
-    for (int i = ind; i < arr.size(); i++)
-    {
-        if (i > ind && arr[i] == arr[i - 1])
-            continue;
-        if (arr[i] > target)
-            break;
-        ds.push_back(arr[i]);
-        findCombination(i + 1, target - arr[i], arr, ans, ds);
-        ds.pop_back();
-    }
-}
-vector<vector<int>> combinationSum2(vector<int> &candidates, int target)
-{
-    sort(candidates.begin(), candidates.end());
-    vector<vector<int>> ans;
-    vector<int> ds;
-    findCombination(0, target, candidates, ans, ds);
-    return ans;
-}
 };
