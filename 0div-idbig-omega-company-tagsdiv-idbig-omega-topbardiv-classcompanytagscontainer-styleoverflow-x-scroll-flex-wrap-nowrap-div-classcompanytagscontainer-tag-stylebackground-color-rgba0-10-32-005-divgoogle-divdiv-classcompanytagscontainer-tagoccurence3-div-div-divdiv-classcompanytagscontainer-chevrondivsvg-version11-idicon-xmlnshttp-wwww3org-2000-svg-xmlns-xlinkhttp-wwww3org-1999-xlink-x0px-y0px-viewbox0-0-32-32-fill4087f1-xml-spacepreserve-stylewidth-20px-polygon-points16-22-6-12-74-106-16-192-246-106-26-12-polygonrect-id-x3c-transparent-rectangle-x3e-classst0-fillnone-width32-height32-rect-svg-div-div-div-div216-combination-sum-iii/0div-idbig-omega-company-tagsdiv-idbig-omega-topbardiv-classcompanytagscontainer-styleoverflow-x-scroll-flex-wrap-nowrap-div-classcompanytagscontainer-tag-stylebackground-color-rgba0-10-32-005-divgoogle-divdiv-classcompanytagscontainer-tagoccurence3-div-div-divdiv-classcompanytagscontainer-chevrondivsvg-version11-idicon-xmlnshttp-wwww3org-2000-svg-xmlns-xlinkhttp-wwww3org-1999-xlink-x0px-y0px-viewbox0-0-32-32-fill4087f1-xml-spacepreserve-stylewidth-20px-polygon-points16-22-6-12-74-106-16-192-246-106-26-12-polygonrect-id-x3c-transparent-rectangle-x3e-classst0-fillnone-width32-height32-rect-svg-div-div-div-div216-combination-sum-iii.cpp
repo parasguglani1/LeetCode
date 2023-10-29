01823f3540b840ddce1e2cf0a3fda39f11069683
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-10-30 01:00:57 +0530

    Time: 0 ms (100.00%), Space: 15.8 MB (5.20%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div216-combination-sum-iii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div216-combination-sum-iii.cpp
@@ -0,0 +1,50 @@
+class Solution
+{
+    public:
+        void helper(int i, vector<int> v, int n, int target, set<vector< int >> &ans, vector< int > temp)
+        {
+            for(int t=0;t<temp.size();t++)
+            {
+                cout<<temp[t]<<' ';
+            }
+            cout<<endl;
+
+            if (temp.size() == n && target == 0)
+            {
+                cout<<"hi"<<endl;
+                ans.insert(temp);
+                return;
+            }
+            if (i >= 9)
+            {
+                return;
+            }
+            if (temp.size() < n && v[i] <= target && v[i]!=0)
+            {
+                helper(i + 1, v, n, target, ans, temp);
+
+                // cout<<v[i]<<' ';
+                temp.push_back(v[i]);
+                target -= v[i];
+                helper(i + 1, v, n, target, ans, temp);
+                // temp.pop_back();
+                // target += v[i];
+            }
+        }
+    vector<vector < int>> combinationSum3(int k, int n)
+    {
+
+        vector<int> v(9);
+        for (int i = 0; i < 9; i++)
+        {
+            v[i] = i + 1;
+        }
+        set<vector<int>> ans;
+        vector<int> temp;
+        int i = 0;
+        helper(i, v, k, n, ans, temp);
+        vector<vector<int> >res;
+        res.assign(ans.begin(), ans.end());
+        return res;
+    }
+};
