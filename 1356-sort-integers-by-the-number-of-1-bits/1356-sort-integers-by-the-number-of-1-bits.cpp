4ad5085af21a02fe8110068d5950db1d7c984ee0
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-10-30 14:22:20 +0530

    Time: 5 ms (79.63%), Space: 11.1 MB (22.36%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
-    static bool cmp(int &a,int &b)
-    {
-        int bit1=__builtin_popcount(a);
-        int bit2=__builtin_popcount(b);
-        if(bit1!=bit2)
-        {
-            return bit1<bit2;
-        }
-        return a<b;
+//     static bool cmp(int &a,int &b)
+//     {
+//         int bit1=__builtin_popcount(a);
+//         int bit2=__builtin_popcount(b);
+//         if(bit1!=bit2)
+//         {
+//             return bit1<bit2;
+//         }
+//         return a<b;
 
-    }
+//     }
     vector<int> sortByBits(vector<int>& arr) {
-     sort(arr.begin(),arr.end(),cmp);
+        sort(arr.begin(),arr.end());
+        int n=arr.size();
+        map<int,vector<int>> mp;
+        for(int i=0;i<n;i++)
+        {
+            mp[__builtin_popcount(arr[i])].push_back(arr[i]);
+        }
+        int i=0;
+        for(auto x:mp)
+        {
+            for(auto y:x.second)
+            arr[i++]=y;
+            
+        }
         return arr;
+     // sort(arr.begin(),arr.end(),cmp);
+        // return arr;
     }
 };
