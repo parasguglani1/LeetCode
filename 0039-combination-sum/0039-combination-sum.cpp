a172fa40f060650fdbe0dffb83f212ed65f74733
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-11-02 03:02:53 +0530

    Time: 50 ms (18.69%), Space: 63.9 MB (12.20%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,45 +1,43 @@
-class Solution {
-public:
+class Solution
+{
+    public:
 
-    void helper(int i,int curr, int target,vector<vector<int>> &ans,vector<int> &temp,vector<int> candidates)
+        void helper(int i, int curr, int target, vector<vector < int>> &ans, vector< int > &temp, vector< int > candidates)
+        {
+            if (i >= candidates.size())
+            {
+                return;
+            }
+           	//base
+            if (curr > target)
+            {
+                return;
+            }
+            if (curr == target)
+            {
+                ans.push_back(temp);
+                return;
+            }
+           	//take
+
+            curr += candidates[i];
+            temp.push_back(candidates[i]);
+            helper(i, curr, target, ans, temp, candidates);
+
+           	//not take
+            curr -= candidates[i];
+            temp.pop_back();
+            helper(i + 1, curr, target, ans, temp, candidates);
+        }
+
+    vector<vector < int>> combinationSum(vector<int> &candidates, int target)
     {
-        if(i>=candidates.size())
-        {
-            return;
-        }
-        //base
-        if(curr>target)
-        {
-            return;
-        }
-        if(curr==target)
-        {
-            ans.push_back(temp);
-            return;
+        vector<vector < int>> ans;
 
-        }
-        //take
-
-
-        curr+=candidates[i];
-        temp.push_back(candidates[i]);
-        helper(i,curr,target,ans,temp,candidates);
-
-        //not take
-        curr-=candidates[i];
-        temp.pop_back();
-        helper(i+1,curr,target,ans,temp,candidates);
-    }
-
-
-
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-
-        //take
-        int curr=0;
+       	//take
+        int curr = 0;
         vector<int> temp;
-        helper(0,curr,target,ans,temp,candidates);
+        helper(0, curr, target, ans, temp, candidates);
         return ans;
     }
 };
