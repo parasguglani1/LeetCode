ea7b618e23524068d2d9865c6db38b619a98286f
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-11-05 02:56:45 +0530

    Time: 16 ms (74.50%), Space: 14 MB (69.28%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& edges) {
+        
+        //do topo sort
+        
+        vector<vector<int>> adj(n);
+        
+        int m=edges.size();
+        vector<int> indegree(n,0);
+        for(int i=0;i<m;i++)
+        {
+            adj[edges[i][1]].push_back(edges[i][0]);
+            indegree[edges[i][0]]++;
+        }
+        queue<int> q;
+        int cnt=0;
+        vector<int> topo;
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.push(i);
+                cnt++;
+            }
+        }
+             
+    
+        while(!q.empty())
+        {
+            int curr=q.front();
+            topo.push_back(curr);
+            q.pop();
+            
+            for(int i=0;i<adj[curr].size();i++)
+            {
+                indegree[adj[curr][i]]--;
+                if(indegree[adj[curr][i]]==0)
+                {
+                    q.push(adj[curr][i]);
+                    cnt++;
+                    
+                }
+                
+            }
+            
+        }
+   vector<int> ans;
+        return cnt==n?topo:ans;        
+        
+        
+        
+    }
+};
