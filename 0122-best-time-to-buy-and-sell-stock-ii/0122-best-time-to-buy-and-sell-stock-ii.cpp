df45d79008702339108d54fe668972a54c035f3f
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-11-12 01:50:08 +0530

    Time: 12 ms (14.02%), Space: 15.6 MB (17.98%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,15 +1,22 @@
-class Solution
-{
-    public:
+class Solution {
+public:
+    int maxProfit(vector<int>& p) {
+        int n = p.size();
+        vector<vector<int>> dp(n+1, vector<int>(2, 0));
+        for(int idx = n-1; idx >= 0; idx--){
 
-        int maxProfit(vector<int> &prices)
-        {
-            int n=prices.size();
-            int profit=0;
-            for(int i=1;i<n;i++)
-            {
-                profit+=max(0,prices[i]-prices[i-1]);
-            }
-            return profit;
+            int b = -p[idx] + dp[idx+1][0];
+
+            int nb = 0 + dp[idx+1][1];
+
+            int s = p[idx] + dp[idx+1][1];
+
+            int ns = 0 + dp[idx+1][0];
+
+            dp[idx][0] = max(s, ns);
+
+            dp[idx][1] = max(b, nb);
         }
+        return dp[0][1];
+    }
 };
