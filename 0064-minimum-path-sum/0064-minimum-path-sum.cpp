3ff570447884318c1388fe785303e0d93178db1c
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-11-12 19:50:02 +0530

    Time: 11 ms (24.05%), Space: 10.5 MB (30.74%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,28 +1,26 @@
-class Solution {
-public:
-    int getmin(int i, int j, vector<vector<int>> &dp, int x)
-    {        
-        if(i==0 and j==0) return x;
-        int n = dp.size(), m = dp[0].size();
-        
-        if(i==0) return dp[i][j-1] + x;
-        if(j==0) return dp[i-1][j] + x;
-        return min(dp[i-1][j], dp[i][j-1]) + x;
-    }
-    
-    int minPathSum(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-        vector<vector<int>> dp(n, vector<int> (m, 0));
-        
-        dp[0][0] = grid[0][0];
-        
-        for (int i = 0;i<n;i++)
+class Solution
+{
+    public:
+
+        int getans(int i, int j, vector<vector < int>> &grid,vector<vector < int>> &dp)
         {
-            for(int j = 0;j<m;j++)
+            if (i == 0 && j == 0) return grid[i][j];
+
+            if (i < 0 || j < 0) return 1e9;
+            if (dp[i][j] != -1)
             {
-                dp[i][j] = getmin(i,j, dp, grid[i][j]);
+                return dp[i][j];
             }
+            int up = getans(i - 1, j, grid,dp);
+            int left = getans(i, j - 1, grid,dp);
+
+            return dp[i][j] = min(up, left) + grid[i][j];
         }
-        return dp[n-1][m-1];
+    int minPathSum(vector<vector < int>> &grid)
+    {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector < int>>dp (n + 1, vector<int> (m + 1, -1));
+
+        return getans(n - 1, m - 1, grid,dp);
     }
 };
