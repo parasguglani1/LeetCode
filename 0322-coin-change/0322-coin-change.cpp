060bdbc5fe35846142494a6094de78cd86660cf9
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-11-14 00:28:33 +0530

    Time: 112 ms (18.40%), Space: 45.4 MB (34.52%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -27,8 +27,33 @@ class Solution
     {
         int n = coins.size();
 
-        vector<vector < int>> dp(n, vector<int> (amount + 1, -1));
-        int ans = getans(coins, n - 1, amount, dp);
+        vector<vector < int>> dp(n, vector<int> (amount + 1, 0));
+       	// int ans = getans(coins, n - 1, amount, dp);
+
+        for (int target = 0; target <= amount; target++)
+        {
+            if (target % coins[0]==0)
+            {
+                dp[0][target] = target / coins[0];
+            }
+            else
+            {
+                dp[0][target] = 1e9;
+            }
+        }
+
+        for (int ind = 1; ind < n; ind++)
+        {
+            for (int target = 0 ; target <= amount; target++)
+            {
+                int take = INT_MAX;
+                if (target >= coins[ind]) take = 1 + dp[ind][target - coins[ind]];
+                int nottake = dp[ind - 1][target];
+                dp[ind][target] = min(take, nottake);
+            }
+        }
+
+       int ans = dp[n - 1][amount];
         if (ans >= 1e9)
         {
             return -1;
