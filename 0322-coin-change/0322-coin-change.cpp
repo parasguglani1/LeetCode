a486b583411f106ab63a1788a4ca2ed602cf9eef
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-11-14 00:14:27 +0530

    Time: 79 ms (49.18%), Space: 45.7 MB (27.58%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,33 +1,35 @@
-class Solution {
-public:
-    int getans(vector<int> &coins,int ind, int target,vector<vector<int>> &dp)
+class Solution
+{
+    public:
+        int getans(vector<int> &coins, int ind, int target, vector<vector< int>> &dp)
+        {
+
+           	//base
+            if (ind == 0)
+            {
+                if (target % coins[0] == 0)
+                    return target / coins[0];
+                return 1e9;
+            }
+
+            if (dp[ind][target] != -1)
+            {
+                return dp[ind][target];
+            }
+
+            int take = INT_MAX;
+            if (target >= coins[ind]) take = 1 + getans(coins, ind, target - coins[ind], dp);
+            int nottake = getans(coins, ind - 1, target, dp);
+            int curans = min(take, nottake);
+            return dp[ind][target] = curans;
+        }
+    int coinChange(vector<int> &coins, int amount)
     {
-    
-        //base
-        if(ind==0)
-        {
-            if (target%coins[0]==0)
-            return target/coins[0];
-            return 1e9;
-        }
-        
-        if(dp[ind][target]!=-1)
-        {
-            return dp[ind][target];
-        }
-        
-        int take=INT_MAX;
-        if(target>=coins[ind]) take= 1+getans(coins,ind,target-coins[ind],dp);
-        int nottake= getans(coins,ind-1,target,dp);
-        int curans=min(take,nottake);
-        return dp[ind][target]=curans;
-    }
-    int coinChange(vector<int>& coins, int amount) {
-        int n=coins.size();
-        
-        vector<vector<int>> dp(n,vector<int> (amount+1,-1));
-        int ans=getans(coins,n-1,amount,dp);
-        if(ans>=1e9)
+        int n = coins.size();
+
+        vector<vector < int>> dp(n, vector<int> (amount + 1, -1));
+        int ans = getans(coins, n - 1, amount, dp);
+        if (ans >= 1e9)
         {
             return -1;
         }
