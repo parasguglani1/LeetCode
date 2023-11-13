7e251ad5e3ff2f989c2f3caa1c6be30d1ab557fe
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-11-14 01:54:29 +0530

    Time: 61 ms (22.41%), Space: 25.2 MB (6.40%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divdoordash-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divkarat-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1143-longest-common-subsequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divdoordash-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divkarat-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1143-longest-common-subsequence.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+    public:
+
+        int getlcs(int i, int j, string &text1, string &text2, vector<vector < int>> &dp)
+        {
+            if (i < 0 || j < 0)
+            {
+                return 0;
+            }
+            if (dp[i][j] != -1)
+            {
+                return dp[i][j];
+            }
+            int ans = 0;
+            if (text1[i] == text2[j])
+            {
+                ans = 1 + getlcs(i - 1, j - 1, text1, text2, dp);
+            }
+            else
+            {
+                ans = max(getlcs(i - 1, j, text1, text2, dp), getlcs(i, j - 1, text1, text2, dp));
+            }
+            return dp[i][j] = ans;
+        }
+
+    int longestCommonSubsequence(string text1, string text2)
+    {
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector < int>> dp(n, vector<int> (m, -1));
+        int ans = getlcs(n - 1, m - 1, text1, text2,dp);
+        return ans;
+    }
+};
