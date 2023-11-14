836c646f131d7522d9144b849bb4b0c75f8231da
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-11-15 01:43:30 +0530

    Time: 13 ms (10.21%), Space: 17.6 MB (11.09%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,24 +1,33 @@
-class Solution {
-public:
-    int maxProfit(vector<int>& p) {
-        int n = p.size();
-        vector<int> next(2, 0), curr(2, 0);
-        for(int idx = n-1; idx >= 0; idx--){
+class Solution
+{
+    public:
+        int getans(int idx, bool buy, vector<int> &prices, vector<vector< int>> &dp)
+        {
+            int n = prices.size();
+            if (idx == n)
+                return 0;
 
-            int b = -p[idx] + next[0];
+            if (dp[idx][buy] != -1)
+                return dp[idx][buy];
 
-            int nb = 0 + next[1];
-
-            int s = p[idx] + next[1];
-
-            int ns = 0 + next[0];
-
-            curr[0] = max(s, ns);
-
-            curr[1] = max(b, nb);
-            
-            next = curr;
+            int ans = 0;
+            if (buy)
+            {
+                int b = -prices[idx] + getans(idx + 1, 0, prices, dp);
+                int nb = 0 + getans(idx + 1, 1, prices, dp);
+                ans = max(b, nb);
+            }
+            else
+            {
+                int s = prices[idx] + getans(idx + 1, 1, prices, dp);
+                int ns = 0 + getans(idx + 1, 0, prices, dp);
+                ans = max(s, ns);
+            }
+            return dp[idx][buy] = ans;
         }
-        return curr[1];
+    int maxProfit(vector<int> &prices)
+    {
+        vector<vector < int>> dp(prices.size(), vector<int> (2, -1));
+        return getans(0, 1, prices, dp);
     }
 };
