a7b68ef49a5e99f79286b7c19eeff0db7cb57e23
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-11-14 20:14:16 +0530

    Time: 25 ms (77.46%), Space: 18.9 MB (51.04%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmathworks-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div115-distinct-subsequences/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmathworks-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div115-distinct-subsequences.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+    public:
+        int getans(int i, int j, string &s, string &t,vector<vector<int>>&dp)
+        {
+
+            if (j < 0)
+            {
+                return 1;
+            }
+            if (i < 0)
+            {
+                return 0;
+            }
+            if(dp[i][j]!=-1)
+            {
+                return dp[i][j];
+            }
+            if (s[i] == t[j])
+            {
+                int taken = getans(i - 1, j - 1, s, t,dp);
+                int nottaken = getans(i - 1, j, s, t,dp);
+                return dp[i][j]= taken + nottaken;
+            }
+
+            return dp[i][j]= getans(i - 1, j, s, t,dp);
+        }
+    int numDistinct(string s, string t)
+    {
+
+        int n = s.size();
+        int m = t.size();
+vector<vector<int>> dp(n,vector<int> (m,-1));
+        int ans = getans(n - 1, m - 1, s, t,dp);
+        return ans;
+    }
+};
