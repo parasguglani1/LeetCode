5ba1e5b39ac86826919a9fd97c4d378c131d649c
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-11-15 00:43:49 +0530

    Time: 58 ms (44.38%), Space: 28.2 MB (24.60%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence14-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divcoursera-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div44-wildcard-matching/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence14-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divcoursera-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div44-wildcard-matching.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence14-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divcoursera-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div44-wildcard-matching/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence14-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divcoursera-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div44-wildcard-matching.cpp
@@ -1,50 +1,84 @@
 class Solution
 {
     public:
-    //memoization
-        bool getans(int i, int j, string &s, string &p, vector<vector < int>> &dp)
-        {
-            if (j == 0 && i == 0)
-            {
-                return true;
-            }
-            if (i > 0 && j== 0)
-            {
-                return false;
-            }
-            if (i == 0 && j > 0)
-            {
-                for (int ind = 1; ind <= j; ind++)
-                {
-                    if (p[ind-1] != '*')
-                        return false;
-                }
-                return true;
-            }
-            if (dp[i][j] != -1)
-            {
-                return dp[i][j];
-            }
+       	//memoization
+//         bool getans(int i, int j, string &s, string &p, vector<vector < int>> &dp)
+//         {
+//             if (j == 0 && i == 0)
+//             {
+//                 return true;
+//             }
+//             if (i > 0 && j == 0)
+//             {
+//                 return false;
+//             }
+//             if (i == 0 && j > 0)
+//             {
+//                 for (int ind = 1; ind <= j; ind++)
+//                 {
+//                     if (p[ind - 1] != '*')
+//                         return false;
+//                 }
+//                 return true;
+//             }
+//             if (dp[i][j] != -1)
+//             {
+//                 return dp[i][j];
+//             }
 
-            if (s[i-1] == p[j-1] || p[j-1] == '?')
-            {
-                return dp[i][j] = getans(i - 1, j - 1, s, p, dp);
-            }
-            if (p[j-1] == '*')
-            {
-                bool possible = false;
-                possible |= getans(i - 1, j, s, p, dp);
-                possible |= getans(i, j - 1, s, p, dp);
-                return dp[i][j] = possible;
-            }
-            return dp[i][j] = false;
-        }
+//             if (s[i - 1] == p[j - 1] || p[j - 1] == '?')
+//             {
+//                 return dp[i][j] = getans(i - 1, j - 1, s, p, dp);
+//             }
+//             if (p[j - 1] == '*')
+//             {
+//                 bool possible = false;
+//                 possible |= getans(i - 1, j, s, p, dp);
+//                 possible |= getans(i, j - 1, s, p, dp);
+//                 return dp[i][j] = possible;
+//             }
+//             return dp[i][j] = false;
+//         }
     bool isMatch(string s, string p)
     {
         int n = s.size();
         int m = p.size();
-        vector<vector < int>> dp(n+1, vector<int> (m+1, -1));
+        vector<vector < int>> dp(n + 1, vector<int> (m + 1, 0));
+        dp[0][0] = 1;
 
-        return getans(n , m, s, p, dp);
+        for (int j = 1; j <= m; j++)
+        {
+            bool flag = true;
+            for (int ind = 1; ind <= j; ind++)
+            {
+                if (p[ind - 1] != '*')
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            dp[0][j] = flag;
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (s[i - 1] == p[j - 1] || p[j - 1] == '?')
+                {
+                    dp[i][j] = dp[i - 1] [j - 1];
+                }
+                else if (p[j - 1] == '*')
+                {
+                    bool possible = false;
+                    possible |= dp[i - 1][ j];
+                    possible |= dp[i] [j - 1];
+                    dp[i][j] = possible;
+                }
+                else dp[i][j] = false;
+            }
+        }
+
+        return dp[n][m];
     }
 };
