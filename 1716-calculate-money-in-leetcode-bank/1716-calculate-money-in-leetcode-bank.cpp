bae01eec23cd97d2bd729215c8fc6dac8f3faa1f
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-12-07 00:24:30 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (98.95%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -1,17 +1,21 @@
-class Solution {
-public:
-    int totalMoney(int n) {
-        int sum=0;
-        int i=0;
-        int day=0;
-        int curr=0;
-        while(curr<n)
+class Solution
+{
+    public:
+        int totalMoney(int n)
         {
-            int week=curr/7+1;
-            int a=curr%7;
-            sum+=a+week;
-            curr++;
+            int sum = 0;
+
+            int times = n / 7;
+
+           	    sum+=times*28;
+           	    times-=1;
+           	    int modsum=(times*(times+1))/2;
+           	    modsum*=7;
+           	    sum+=modsum;
+            int left = (n) % 7;
+            sum += (times+1) * left;
+            sum += (left *(left + 1)) / 2;
+
+            return sum;
         }
-        return sum;
-    }
 };
