318086e35a0e3dab76d0680508f526aca277b40f
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-12-24 12:37:02 +0530

    Time: 3 ms (78.34%), Space: 7.3 MB (45.10%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -2,27 +2,16 @@ class Solution {
 public:
     int minOperations(string s) {
         int n=s.size();
-        
-        string str1(n,'1'),str2(n,'0');
-        
-        for(int i=1;i<n;i+=2)
+        int cnt=0;
+        for(int i=1;i<n;i++)
         {
-            str1[i]='0';
-            str2[i]='1';
-        }
-        int cnt1=0,cnt2=0;
-        for(int i=0;i<n;i++)
-        {
-            if(s[i]!=str1[i])
+            if(s[i]==s[i-1])
             {
-                cnt1++;
-            }
-            if(s[i]!=str2[i])
-            {
-                cnt2++;
+                cnt++;
+                s[i]='1'-s[i]+'0';
             }
         }
-        return min(cnt2,cnt1);
-        // cout<<str1<<' '<<str2<<endl;;
+        // cout<<s<<endl;
+        return min(cnt,n-cnt);
     }
 };
