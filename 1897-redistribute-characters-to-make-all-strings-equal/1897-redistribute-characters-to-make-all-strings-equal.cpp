bfa555a713d857a05f6bbf7a09a708431ba9d5a6
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2023-12-30 12:30:51 +0530

    Time: 22 ms (12.84%), Space: 12.8 MB (10.12%) - LeetHub

---
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+        vector<int> freq(26,0);
+        
+        int n=words.size();
+        
+        for(auto x:words)
+        {
+            for(auto y:x)
+            {
+                freq[y-'a']++;
+            }
+        }
+        
+        for(auto x:freq)
+        {
+            
+            if(x%n!=0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
