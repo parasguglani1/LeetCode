80a362b66aea845bdeb678a15f76279e297df59b
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2024-06-15 12:31:03 +0530

    Time: 130 ms (87.46%), Space: 81.5 MB (89.17%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence5-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div502-ipo/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence5-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div502-ipo.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+    public:
+
+        int findMaximizedCapital(int k, int w, vector<int> &profits, vector<int> &capital)
+        {
+
+            int n = capital.size();
+            vector<pair<int, int>> vp(n);
+            for (int i = 0; i < n; i++)
+            {
+                vp[i] = { capital[i],
+                    profits[i]
+                };
+            }
+            sort(vp.begin(), vp.end());
+            int i = 0;
+            priority_queue<int> pq;
+            while (k--)
+            {
+                while (i < n && vp[i].first <= w)
+                {
+                    pq.push(vp[i++].second);
+                }
+                if (pq.empty())
+                {
+                    break;
+                }
+                w += pq.top();
+                pq.pop();
+            }
+            return w;
+        }
+};
