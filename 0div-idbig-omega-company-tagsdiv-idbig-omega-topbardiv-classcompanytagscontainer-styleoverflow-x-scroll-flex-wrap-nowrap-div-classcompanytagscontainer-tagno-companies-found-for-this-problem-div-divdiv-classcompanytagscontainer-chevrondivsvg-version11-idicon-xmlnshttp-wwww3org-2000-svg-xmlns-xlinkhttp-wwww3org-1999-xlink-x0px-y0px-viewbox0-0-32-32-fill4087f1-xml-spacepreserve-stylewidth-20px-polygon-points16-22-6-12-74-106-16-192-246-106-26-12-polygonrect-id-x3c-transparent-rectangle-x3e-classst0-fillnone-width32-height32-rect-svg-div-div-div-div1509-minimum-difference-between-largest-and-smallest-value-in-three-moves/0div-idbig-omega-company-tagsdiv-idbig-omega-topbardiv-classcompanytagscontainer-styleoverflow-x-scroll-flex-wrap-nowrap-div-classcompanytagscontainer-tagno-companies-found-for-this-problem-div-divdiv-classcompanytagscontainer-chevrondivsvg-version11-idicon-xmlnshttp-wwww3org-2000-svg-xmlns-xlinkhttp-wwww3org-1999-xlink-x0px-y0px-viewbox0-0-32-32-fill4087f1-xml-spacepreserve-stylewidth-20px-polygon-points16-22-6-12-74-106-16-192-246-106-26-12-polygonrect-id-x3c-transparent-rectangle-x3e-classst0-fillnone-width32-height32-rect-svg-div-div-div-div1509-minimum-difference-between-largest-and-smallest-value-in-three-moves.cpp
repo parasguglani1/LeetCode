bb45f8b333a41a787e91a89a755042f2f3b02770
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2024-07-03 11:57:29 +0530

    Time: 180 ms (5.05%), Space: 60 MB (5.30%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minDifference(vector<int>& nums) {
+        int n=nums.size();
+
+        set<int> s;
+        for(auto x: nums)
+        {
+            s.insert(x);
+        }
+        if(n<5)
+        {
+            return 0;
+        }
+        sort(nums.begin(),nums.end());
+        // 4 and last
+        int ans=nums[n-1]-nums[3];
+        int ans2=nums[n-2]-nums[2];
+        int ans3=nums[n-3]-nums[1];
+        int ans4=nums[n-4]-nums[0];
+        
+        return min({ans,ans2,ans3,ans4});
+    }
+};
+
+
+// 1  4 4 5 9 
