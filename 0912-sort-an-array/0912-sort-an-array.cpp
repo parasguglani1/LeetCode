3c581efb806aa042eea1678fef327651487e2dcf
Paras Guglani <73358562+parasguglani1@users.noreply.github.com>
2024-07-25 17:09:07 +0530

    Time: 313 ms (39.89%), Space: 142.2 MB (42.52%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,16 +1,58 @@
 class Solution
 {
     public:
-        vector<int> sortArray(vector<int> &nums)
+        void merge(vector<int> &a, int low,int mid,int high)
         {
-            priority_queue<int, vector < int>, greater < int>> pq;
-            for (int i: nums)
-                pq.push(i);
-            for (int i = 0; i < nums.size(); i++)
+            int n = mid, m = high;
+            int i = low, j = mid+1, k = 0;
+            vector<int> ans(high-low+1);
+            while (i <= n && j <= m)
             {
-                nums[i] = pq.top();
-                pq.pop();
+                if (a[i] < a[j])
+                {
+                    ans[k++] = a[i++];
+                }
+                else
+                {
+                    ans[k++] = a[j++];
+                }
             }
-            return nums;
+
+            while (i <= n)
+            {
+                ans[k++] = a[i++];
+            }
+            while (j <= m)
+            {
+                ans[k++] = a[j++];
+            }
+            for(int i=low;i<=high;i++)
+            {
+                a[i]=ans[i-low];
+            }
         }
+    
+    
+    void mergeSort(vector<int> &nums,int low,int high)
+    {
+        if(low>=high)
+        {
+            return;
+        }
+        int mid=(high-low)/2+low;
+        mergeSort(nums,low,mid);
+        mergeSort(nums,mid+1,high);
+        merge(nums,low,mid,high);
+        
+        
+    }
+    vector<int> sortArray(vector<int> &nums)
+    {
+       	//MERGE SORT
+        mergeSort(nums,0,nums.size()-1);
+        return nums;
+    
+        
+        
+    }
 };
